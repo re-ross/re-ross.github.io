4bb7dbc28c21e0649ea38625984093e654293311
Ryan Ross <ryan@reross.com>
2022-03-09 12:31:34 -0600

    updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row } from 'react-bootstrap';
-// import { ThemeContext } from 'styled-components';
-import PropTypes from 'prop-types';
-import Fade from 'react-reveal/Fade';
-import Header from './Header';
-import endpoints from '../constants/endpoints';
-import ProjectCard from './projects/ProjectCard';
-import FallbackSpinner from './FallbackSpinner';
+/* eslint-disable quotes */
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import PropTypes from "prop-types";
+import Fade from "react-reveal/Fade";
+import Header from "./Header";
+import endpoints from "../constants/endpoints";
+import ProjectCard from "./projects/ProjectCard";
+import FallbackSpinner from "./FallbackSpinner";
 
 const styles = {
   containerStyle: {
@@ -23,7 +23,7 @@ const Projects = (props) => {
 
   useEffect(() => {
     fetch(endpoints.projects, {
-      method: 'GET',
+      method: "GET",
     })
       .then((res) => res.json())
       .then((res) => setData(res))
@@ -33,20 +33,21 @@ const Projects = (props) => {
   return (
     <>
       <Header title={header} />
-      {data
-        ? (
-          <div className="section-content-container">
-            <Container style={styles.containerStyle}>
-              <Row xs={1} sm={1} md={2} lg={3} className="g-4">
-                {data.projects?.slice(0, numberOfItems).map((project) => (
-                  <Fade key={project.title}>
-                    <ProjectCard project={project} />
-                  </Fade>
-                ))}
-              </Row>
-            </Container>
-          </div>
-        ) : <FallbackSpinner /> }
+      {data ? (
+        <div className="section-content-container">
+          <Container style={styles.containerStyle}>
+            <Row xs={1} sm={1} md={2} lg={3} className="g-4">
+              {data.projects?.slice(0, numberOfItems).map((project) => (
+                <Fade key={project.title}>
+                  <ProjectCard project={project} />
+                </Fade>
+              ))}
+            </Row>
+          </Container>
+        </div>
+      ) : (
+        <FallbackSpinner />
+      )}
     </>
   );
 };
