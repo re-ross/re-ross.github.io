7d70cd403d710c5ccba02978a70ce6bbba8299a6
Ryan Ross <ryan@reross.com>
2022-04-07 08:56:11 -0500

    Updates

---
+++ static/js/5.3578539e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[5],{87:function(t,e,p){"use strict";p.r(e);p(0),p(89);var a=p(1);e.default=function(t){var e=t.title;return Object(a.jsx)("div",{className:"header",children:e})}}}]);
+//# sourceMappingURL=5.3578539e.chunk.js.map
